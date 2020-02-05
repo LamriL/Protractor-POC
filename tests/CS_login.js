faace58310a4ffabf9d81ee66c98fee43ed810d5
mendieta69 <lamrinx@msn.com>
2020-02-05 17:25:52 +0100

    tessst

--- tests/CS_login.js
+++ tests/CS_login.js
@@ -1,8 +1,8 @@
 let login = require('../pages/CS_login_homepage');
 
-describe('demo calculator tests', function () {
+describe('ChamberSign - Integ', function () {
 
-    it('addition test', function () {
+    it('Login', function () {
 
         login.get('http://integ.chambersign.apsdigit.lan/viseo/view/fr/connexion');
 
@@ -14,5 +14,29 @@ describe('demo calculator tests', function () {
 
     });
 
+    if (expect(element(by.css('[placeholder = "9 chiffres exactement"]')).isPresent()).toBe(true))
+    {
+        console.log("PRESENT");
+    }
+    else
+        console.log("ABSENT");
 
+
+
+     /*   
+    it('Organisation', function () {
+
+        login.get('http://integ.chambersign.apsdigit.lan/viseo/view/fr/connexion');
+
+        login.enterEmail('lamri123@yopmail.com');
+        login.enterPassword('Azerty1;')
+        login.connectGo();
+
+        element(by.css('[placeholder = "9 chiffres exactement"]'));]
+
+
+        browser.sleep(2000);
+
+    });
+*/
 });
